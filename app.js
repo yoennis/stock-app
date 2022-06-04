d7b583fc0004437542b891292423fae7b7b91850
Yoe Vargas <Yoe.Vargas@neudesic.com>
2022-06-03 21:16:04 -0700

    Updating app PORT

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
-
 app.get('/', (req, res) => {
     console.log('Request received');
 
     res.send('<h1>Hello, world!</h1>');
 });
 
-app.listen(4000, () => {
-    console.log('Servidor listening on port 4000');
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Servidor listening on port ${PORT}`);
 });
